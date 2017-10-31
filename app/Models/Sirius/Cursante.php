5f3a52fb289ac052cd8fd40b29ce24f1b7af5475
Misaji95 <misaji95@hotmail.com>
2017-10-31 00:16:25 -0500

    mostrando todos los horarios en /api/horarios

---
+++ app/Models/Sirius/Cursante.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Tue, 31 Oct 2017 01:02:18 +0000.
+ */
+
+namespace App\Models\Sirius;
+
+use Reliese\Database\Eloquent\Model as Eloquent;
+
+class Cursante extends Eloquent
+{
+	public $incrementing = false;
+	public $timestamps = false;
+
+	protected $table ="cursantes";
+
+
+}
---
+++ app/Models/Sirius/Schedule.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Tue, 31 Oct 2017 03:49:50 +0000.
+ */
+
+namespace App\Models\Sirius;
+
+use Reliese\Database\Eloquent\Model as Eloquent;
+
+
+class Schedule extends Eloquent
+{
+	public $incrementing = false;
+	public $timestamps = false;
+
+	protected $table = 'schedules';
+
+
+}
